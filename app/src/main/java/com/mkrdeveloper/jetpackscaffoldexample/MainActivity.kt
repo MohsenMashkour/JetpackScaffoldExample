ef467ee296f741085e7349729b11b1690d905e38
Mohsen Mashkour <49885171+MohsenMashkour@users.noreply.github.com>
2023-08-12 12:02:43 +0200

    navigation bar

---
+++ app/src/main/java/com/mkrdeveloper/jetpackscaffoldexample/MainActivity.kt
@@ -0,0 +1,169 @@
+package com.mkrdeveloper.jetpackscaffoldexample
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Face
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mkrdeveloper.jetpackscaffoldexample.ui.theme.JetpackScaffoldExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackScaffoldExampleTheme {
+                MyApp()
+            }
+        }
+    }
+}
+
+data class NavItemState(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val hasBadge : Boolean,
+    val messages : Int
+)
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    val items = listOf(
+        NavItemState(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home,
+            hasBadge = false,
+            messages = 0
+        ),
+        NavItemState(
+            title = "Inbox",
+            selectedIcon = Icons.Filled.Email,
+            unselectedIcon = Icons.Outlined.Email,
+            hasBadge = false,
+            messages = 12
+        ),
+        NavItemState(
+            title = "Account",
+            selectedIcon = Icons.Filled.Face,
+            unselectedIcon = Icons.Outlined.Face,
+            hasBadge = true,
+            messages = 0
+        )
+    )
+    var bottomNavState by rememberSaveable {
+        mutableStateOf(0)
+    }
+    Scaffold(
+
+        topBar = {
+            TopAppBar(title = { Text(text = "Top bar") })
+        },
+        bottomBar = {
+            NavigationBar(
+                modifier
+                    .padding(10.dp)
+                    .clip(RoundedCornerShape(20.dp)),
+                containerColor = Color(0xFFE0A9A5)
+            ) {
+                items.forEachIndexed { index, item ->
+
+
+                    NavigationBarItem(
+
+                        selected = bottomNavState == index,
+                        onClick = { bottomNavState = index },
+                        icon = {
+                            BadgedBox(badge = {
+                                if (item.hasBadge) Badge {}
+                                if (item.messages != 0 ) Badge {
+                                    Text(text = "${item.messages}")
+                                }
+                            }) {
+                                Icon(
+                                    imageVector = if (bottomNavState == index) item.selectedIcon
+                                    else item.unselectedIcon,
+                                    contentDescription = item.title
+                                )
+                            }
+
+                        },
+                        label = { Text(text = item.title) }
+                        , colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = Color(0xFF552A27),
+                            selectedTextColor = Color(0xFF63332F),
+                            indicatorColor = Color(0xFFBB7E7A)
+                        )
+                    )
+                }
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { }) {
+                Icon(imageVector = Icons.Filled.Add, contentDescription = "add")
+            }
+        }
+    ) { contentPadding ->
+
+        Column(modifier.padding(contentPadding)
+            .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = items[bottomNavState].title, fontSize = 44.sp, fontWeight = FontWeight.Bold)
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackScaffoldExampleTheme {
+        MyApp()
+    }
+}
